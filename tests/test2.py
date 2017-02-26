87571a8e4640c8cde2fd6e77cc0632f1380c1cc2
JosephJamesDoyle87 <JosephJamesDoyle87@gmail.com>
2017-02-26 11:36:23 +0000

    slicing 2

--- tests/test2.py
+++ tests/test2.py
@@ -1,6 +1,6 @@
 # import pprint
 # 
-# a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 # print (a)
 
 # a2d = [a, a, a, a]
@@ -14,8 +14,12 @@
 # N =122
 # a2d = [ list(range(i*N, i*N + N)) for i in range(N) ]
 # print(a2d)
+# 
+# slicing 
+# N = 999
+# a = list(range(N))
+# print(a[99:105])
 
-#slicing 
-N = 999
-a = list(range(N))
-print(a[99:105])
+#slicing 2
+
+print(a[2:5])
