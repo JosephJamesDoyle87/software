d7a7d74c198f70273496611fa3814c792dc2fddb
JosephJamesDoyle87 <JosephJamesDoyle87@gmail.com>
2017-02-26 13:39:39 +0000

    testing pprint method
    Signed-off-by: JosephJamesDoyle87 <JosephJamesDoyle87@gmail.com>

--- tests/test3.py
+++ tests/test3.py
@@ -1,13 +1,31 @@
+from pprint import pprint
+
+stuff = ["a",'b','c']
+
+pprint (stuff)
+
 from builtins import str
 def grid(str):
     "This prints a passed string into the function"
     print (str)
     return;
 
+print('\n')
 grid("This is a grid")
 print('\n')
 grid("This is another grid")
 print('\n')
 grid("and this is another grid")
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+pprint (a)
+    
+a2d = [a, a, a, a]
+pprint(a2d)
+[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+ [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+ [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+ [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]
+    
 
     
